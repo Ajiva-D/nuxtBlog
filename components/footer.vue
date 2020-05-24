8e978b417a46440b6146b5c6f2cbc724f5e9585b
David Aji <codekago@David-Ajis-MacBook-Pro.local>
2020-05-24 13:50:40 -0700

    created footer

---
+++ components/footer.vue
@@ -0,0 +1,30 @@
+<template>
+	<footer class="px-5 d-flex justify-content-center">
+	<div class="text-center">
+			<div>
+			Made with ❤️ by <a href="https://david-aji.com/" target="_blank" rel="noopener noreferrer">David Aji</a>
+		</div>
+		<div>
+			<p class="small">
+				This project uses <a href="http://nuxtjs.org" target="_blank" rel="noopener noreferrer">Nuxt</a>
+				and <a href="https://content.nuxtjs.org/" target="_blank" rel="noopener noreferrer">Nuxt content module</a>
+			</p>
+		</div>
+	</div>
+	</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+footer{
+	font-size:18px;
+	a{
+		color:rgba(141, 20, 14, 1);
+	}
+}
+</style>
